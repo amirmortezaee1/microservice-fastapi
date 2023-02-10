cb278fbd51809dc975d37e435b11d67c8ec2833f
amirmortezaee1 <amir.mortezaee1@gmail.com>
2023-02-10 23:48:27 +0330

    add crud application

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
+# uvicorn app.main:app --reload
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from redis_om import get_redis_connection, HashModel
 
 app = FastAPI()
 
-# app.add_middleware()
+origins = [
+    'http://localhost:3000',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_methods=['*'],
+    allow_headers=['*'],
+    )
 
 redis = get_redis_connection(
     host="redis-15431.c300.eu-central-1-1.ec2.cloud.redislabs.com:15431",
@@ -20,8 +30,28 @@ class Product(HashModel):
     class Meta:
         database = redis 
 
-
 @app.get("/products")
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
 
+def format(pk: str):
+    products = Product.get(pk)
+
+    return{
+        'id': products.id,
+        'name': products.name,
+        'price': products.price,
+        'quantity': products.quantity,
+    }
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
+
+@app.get("/products/{pk}")
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete("/products/{pk}")
+def delete(pk: str):
+    return Product.delete(pk)
